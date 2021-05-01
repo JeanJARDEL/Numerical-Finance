f14bfe71633770247ad3fd09b32859d359ac5fc2
JeanJARDEL <jean.jardel76@gmail.com>
2021-05-01 17:47:21 +0200

    Started brownian_nd implementation, doesn't work for now

--- finite_difference/brownian_nd.cpp
+++ finite_difference/brownian_nd.cpp
@@ -4,7 +4,25 @@
 BrownianND::BrownianND(Normal* gen, int dim, Matrix<double> cor) : RandomProcess(gen, dim), correlation_matrix(cor) {};
 
 void BrownianND::simulate(double start_time, double end_time, size_t nb_steps) {
-    // To be implemented
+    int nb_time_steps = paths[0]->get_time_step();
+    Matrix<double> cholesky = correlation_matrix.cholesky();
+    printf("%s", cholesky.to_string().c_str());
+    Matrix<double> draws(dimension, nb_steps);
+    printf("%s", draws.to_string().c_str());
+    for(int i = 0; i < dimension; i++)
+        for(int j = 0; j < nb_steps; j++)
+            draws.set_elem_at(i, j, generator->generate());
+    printf("%s", draws.to_string().c_str());
+    
+    Matrix<double> prod(dimension, nb_steps);
+    prod = cholesky.dot(draws);
+    printf("%s", prod.to_string().c_str());
+    prod = prod * sqrt(nb_time_steps);
+    printf("%s", prod.to_string().c_str());
+
+    for(int i = 0; i < dimension; i++)
+        for(int j = 0; j < nb_steps; j++)
+            paths[i]->add_value(prod.elem_at(i, j));
 }
 
 BrownianND::~BrownianND() {};
